81ec4a64883a1365788469fd26aa640c24baf366
pcbaldwin <pbaldwin@clearfoundation.com>
2014-12-11 14:41:32 -0500

    Prepping for snapshot build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'mail_filter';
-$app['version'] = '2.0.1';
+$app['version'] = '2.0.5';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
