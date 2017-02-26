3508ec633cb8fdc0859a5975e9c9fffe6c97ef49
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:12:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'मेल फिल्टर इन्जिन इनबाउन्ड र बहिर्गमन मेल सन्देशहरू फिल्टर लागि उपकरण को एक कोर सेट प्रदान गर्दछ।';
+$lang['mail_filter_app_name'] = 'मेल फिल्टर इन्जिन';
+$lang['mail_filter_bad_header_policy'] = 'खराब हेडर नीति';
+$lang['mail_filter_banned_file_extensions'] = 'प्रतिबन्धित फाइल एक्सटेन्सन';
+$lang['mail_filter_banned_file_extension_policy'] = 'प्रतिबन्धित फाइल विस्तार नीति';
+$lang['mail_filter_discard_policy'] = 'नीति खारेज';
+$lang['mail_filter_file_extension'] = 'फाइल विस्तार';
+$lang['mail_filter_mail_volume'] = 'मेल मात्रा';
+$lang['mail_filter_quarantine'] = 'क्वरेंटिन';
+$lang['mail_filter_quarantine_policy'] = 'क्वरेंटिन नीति';
+$lang['mail_filter_subject_tag'] = 'विषय ट्याग';
+$lang['mail_filter_subject_tag_invalid'] = 'विषय ट्याग अमान्य छ।';
+$lang['mail_filter_virus_detected_policy'] = 'पत्ता भाइरस नीति';
