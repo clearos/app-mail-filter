9c37f9090656a8d0d50d9bd218efc6d1019959f1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:15:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Posta Filtresi Motor gelen ve giden posta iletilerini filtreleme için bir araç çekirdek kümesi sağlar.';
+$lang['mail_filter_app_name'] = 'Posta Filtresi Motor';
+$lang['mail_filter_bad_header_policy'] = 'Kötü Üstbilgi Politikası';
+$lang['mail_filter_banned_file_extensions'] = 'Yasak Dosya Uzantıları';
+$lang['mail_filter_banned_file_extension_policy'] = 'Yasak Dosya Uzantısı Politikası';
+$lang['mail_filter_discard_policy'] = 'Sil Politikası';
+$lang['mail_filter_file_extension'] = 'Dosya uzantısı';
+$lang['mail_filter_mail_volume'] = 'posta Hacmi';
+$lang['mail_filter_quarantine'] = 'Karantina';
+$lang['mail_filter_quarantine_policy'] = 'karantina Politikası';
+$lang['mail_filter_subject_tag'] = 'Konu Etiketi';
+$lang['mail_filter_subject_tag_invalid'] = 'Konu etiketi geçersiz.';
+$lang['mail_filter_virus_detected_policy'] = 'Algılanan virüs Politikası';
