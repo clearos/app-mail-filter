dc104705a9d9509d4e975eb80c7872abe662c90a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:08:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'The Mail Filter Engine samar da wani core sa na kayayyakin aiki, don tace inbound da outbound mail saƙonni.';
+$lang['mail_filter_app_name'] = 'Mail Filter Engine';
+$lang['mail_filter_bad_header_policy'] = 'Bad Header Policy';
+$lang['mail_filter_banned_file_extensions'] = 'Dakatar File Extensions';
+$lang['mail_filter_banned_file_extension_policy'] = 'Dakatar File Tsawo Policy';
+$lang['mail_filter_discard_policy'] = 'Watsar Policy';
+$lang['mail_filter_file_extension'] = 'file Tsawo';
+$lang['mail_filter_mail_volume'] = 'Mail Volume';
+$lang['mail_filter_quarantine'] = 'keɓe masu ciwo';
+$lang['mail_filter_quarantine_policy'] = 'keɓe masu ciwo Policy';
+$lang['mail_filter_subject_tag'] = 'subject Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'Subject tag ba daidai ba ne.';
+$lang['mail_filter_virus_detected_policy'] = 'Gano cutar Policy';
