a3d9f4727501a3aa43e2ed0ea89cda7aa0299fe8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:07:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Mail Filter Engine tarjoaa ydinjoukko työkaluja suodattamalla saapuvan ja lähtevän postin viestejä.';
+$lang['mail_filter_app_name'] = 'Mail Filter Engine';
+$lang['mail_filter_bad_header_policy'] = 'Bad Header Policy';
+$lang['mail_filter_banned_file_extensions'] = 'Kielletyt File Extensions';
+$lang['mail_filter_banned_file_extension_policy'] = 'Kielletyt Tiedostotunnus Policy';
+$lang['mail_filter_discard_policy'] = 'poisheittämispolitiikan';
+$lang['mail_filter_file_extension'] = 'Tiedostopääte';
+$lang['mail_filter_mail_volume'] = 'mail Volume';
+$lang['mail_filter_quarantine'] = 'karanteeni';
+$lang['mail_filter_quarantine_policy'] = 'Quarantine Policy';
+$lang['mail_filter_subject_tag'] = 'Aihe Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'Aihe tag on virheellinen.';
+$lang['mail_filter_virus_detected_policy'] = 'Havaitut Virus Policy';
