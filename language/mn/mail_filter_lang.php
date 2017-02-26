4dbe75545299b083bc6f788ff1e8040d2678640c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:11:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Захидал шүүлтүүр Engine ирж байгаа болон гарч мэйл мессеж шүүлт зориулагдсан хэрэгслүүдийн үндсэн багц олгодог.';
+$lang['mail_filter_app_name'] = 'Захидал шүүлтүүр Engine';
+$lang['mail_filter_bad_header_policy'] = 'Муу Толгой бодлого';
+$lang['mail_filter_banned_file_extensions'] = 'Хориглосон Файл Өргөтгөлүүд';
+$lang['mail_filter_banned_file_extension_policy'] = 'Хориглосон Файл Өргөтгөл бодлого';
+$lang['mail_filter_discard_policy'] = 'хэрэгсэхгүй болгох бодлого';
+$lang['mail_filter_file_extension'] = 'файл Өргөтгөл';
+$lang['mail_filter_mail_volume'] = 'Mail-р боть';
+$lang['mail_filter_quarantine'] = 'хорио цээр';
+$lang['mail_filter_quarantine_policy'] = 'хорио цээрийн бодлого';
+$lang['mail_filter_subject_tag'] = 'Агуулга Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'Агуулга хаяг хүчин төгөлдөр бус байна.';
+$lang['mail_filter_virus_detected_policy'] = 'Илэрсэн Virus бодлого';
