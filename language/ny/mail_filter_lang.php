4dce4a73f9a2070625d6e70c6080d516bb06c829
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:06:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'The Mail Woganiza Engine imapereka maziko ya zida imathandiza mauthenga inbound ndi outbound makalata.';
+$lang['mail_filter_app_name'] = 'Mail Woganiza Engine';
+$lang['mail_filter_bad_header_policy'] = 'Bad Policy maonekedwe';
+$lang['mail_filter_banned_file_extensions'] = 'Zoletsedwa Buku yophunzitsa';
+$lang['mail_filter_banned_file_extension_policy'] = 'Zoletsedwa Buku Policy Extension';
+$lang['mail_filter_discard_policy'] = 'kutaya Policy';
+$lang['mail_filter_file_extension'] = 'wapamwamba Extension';
+$lang['mail_filter_mail_volume'] = 'Mail Volume';
+$lang['mail_filter_quarantine'] = 'kwaokha';
+$lang['mail_filter_quarantine_policy'] = 'kwaokha Policy';
+$lang['mail_filter_subject_tag'] = 'phunziro Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'Phunziro opatsidwa ndi opanda pake.';
+$lang['mail_filter_virus_detected_policy'] = 'Wapezeka Virus Policy';
