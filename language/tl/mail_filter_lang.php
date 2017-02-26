b1c733dd00506d959d11b19d9131cd3bd7e4ab5e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:15:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Ang Mail Filter Engine ay nagbibigay ng isang pangunahing hanay ng mga tool para sa pag-filter ng inbound at outbound na mensahe mail.';
+$lang['mail_filter_app_name'] = 'Mail Filter Engine';
+$lang['mail_filter_bad_header_policy'] = 'Bad Patakaran Header';
+$lang['mail_filter_banned_file_extensions'] = 'Pinagbawalan Extension File';
+$lang['mail_filter_banned_file_extension_policy'] = 'Pinagbawalan File Patakaran Extension';
+$lang['mail_filter_discard_policy'] = 'Itapon Patakaran';
+$lang['mail_filter_file_extension'] = 'file Extension';
+$lang['mail_filter_mail_volume'] = 'mail Volume';
+$lang['mail_filter_quarantine'] = 'kuwarentenas';
+$lang['mail_filter_quarantine_policy'] = 'Quarantine Patakaran';
+$lang['mail_filter_subject_tag'] = 'subject Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'Subject tag ay hindi wasto.';
+$lang['mail_filter_virus_detected_policy'] = 'Nakita Patakaran Virus';
