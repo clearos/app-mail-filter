c37fb30c369e2f4e6511abe8d9cd530b3cc23e99
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:09:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'The Mail Filter Vél veitir algerlega sett af verkfærum til að sía heimleið og útleið tölvupóst.';
+$lang['mail_filter_app_name'] = 'Mail Filter Engine';
+$lang['mail_filter_bad_header_policy'] = 'Bad haus Stefna';
+$lang['mail_filter_banned_file_extensions'] = 'Bannaður File Extensions';
+$lang['mail_filter_banned_file_extension_policy'] = 'Bannaður skráarnafnsending Policy';
+$lang['mail_filter_discard_policy'] = 'Fleygja Stefna';
+$lang['mail_filter_file_extension'] = 'skrá Eftirnafn';
+$lang['mail_filter_mail_volume'] = 'mail Volume';
+$lang['mail_filter_quarantine'] = 'Quarantine';
+$lang['mail_filter_quarantine_policy'] = 'Quarantine Stefna';
+$lang['mail_filter_subject_tag'] = 'efni Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'Efni tag er ógilt.';
+$lang['mail_filter_virus_detected_policy'] = 'Fundin Veira Stefna';
