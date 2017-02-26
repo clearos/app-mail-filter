e20555061583b4ed5266f5a70e1894ac0673d4e0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:09:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/it/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Il motore di posta Filter fornisce un insieme di strumenti per filtrare i messaggi di posta in entrata e in uscita.';
+$lang['mail_filter_app_name'] = 'Mail Filter Engine';
+$lang['mail_filter_bad_header_policy'] = 'Politica Intestazione Bad';
+$lang['mail_filter_banned_file_extensions'] = 'Estensioni di file Banned';
+$lang['mail_filter_banned_file_extension_policy'] = 'Banned privacy File extension';
+$lang['mail_filter_discard_policy'] = 'Politica Scartare';
+$lang['mail_filter_file_extension'] = 'Estensione del file';
+$lang['mail_filter_mail_volume'] = 'posta Volume';
+$lang['mail_filter_quarantine'] = 'Quarantena';
+$lang['mail_filter_quarantine_policy'] = 'Politica di quarantena';
+$lang['mail_filter_subject_tag'] = 'soggetto Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'Soggetto tag non è valido.';
+$lang['mail_filter_virus_detected_policy'] = 'Politica di virus rilevato';
