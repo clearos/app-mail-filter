096a62778126f9615f236aad0183cabc5bcfd2af
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:12:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'The Mail Filter Engine menyediakan satu set teras alat untuk menapis mesej mel masuk dan keluar.';
+$lang['mail_filter_app_name'] = 'Mail Filter Engine';
+$lang['mail_filter_bad_header_policy'] = 'Polisi Tandukan Bad';
+$lang['mail_filter_banned_file_extensions'] = 'Sambungan Fail Diharamkan';
+$lang['mail_filter_banned_file_extension_policy'] = 'Diharamkan Dasar Extension fail';
+$lang['mail_filter_discard_policy'] = 'Polisi Buang';
+$lang['mail_filter_file_extension'] = 'Extension fail';
+$lang['mail_filter_mail_volume'] = 'Volume mail';
+$lang['mail_filter_quarantine'] = 'kuarantin';
+$lang['mail_filter_quarantine_policy'] = 'Polisi kuarantin';
+$lang['mail_filter_subject_tag'] = 'subjek tag';
+$lang['mail_filter_subject_tag_invalid'] = 'tag tersebut adalah tidak sah.';
+$lang['mail_filter_virus_detected_policy'] = 'Polisi Virus Dikesan';
