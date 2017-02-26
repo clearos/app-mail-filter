ba2899c682d7f82e27b4be8d8757ed0361aeb220
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:11:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Te Engine Tātari Mail whakarato te huinga matua o taputapu mō te tātari karere mēra tākairoto me arawaho.';
+$lang['mail_filter_app_name'] = 'Engine Tātari mēra';
+$lang['mail_filter_bad_header_policy'] = 'Kaupapahere Pane kino';
+$lang['mail_filter_banned_file_extensions'] = 'Toronga Kōnae rāhuitia';
+$lang['mail_filter_banned_file_extension_policy'] = 'Kaupapahere Toronga Kōnae rāhuitia';
+$lang['mail_filter_discard_policy'] = 'Kaupapahere Whiua';
+$lang['mail_filter_file_extension'] = 'Toronga kōnae';
+$lang['mail_filter_mail_volume'] = 'Rōrahi mēra';
+$lang['mail_filter_quarantine'] = 'taratahi';
+$lang['mail_filter_quarantine_policy'] = 'Kaupapahere taratahi';
+$lang['mail_filter_subject_tag'] = 'Tūtohu Kaupapa';
+$lang['mail_filter_subject_tag_invalid'] = 'He muhu Kaupapa tūtohu.';
+$lang['mail_filter_virus_detected_policy'] = 'Kaupapahere Virus Kitea';
