d4c104c8967ca7186aeafc5a3533c5a23bc193c9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:05:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bg/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Двигателят на Mail Филтър осигурява основен набор от инструменти за филтриране на входящите и изходящите съобщения поща.';
+$lang['mail_filter_app_name'] = 'Mail Филтър Engine';
+$lang['mail_filter_bad_header_policy'] = 'Bad политика Header';
+$lang['mail_filter_banned_file_extensions'] = 'Забранени файлови разширения';
+$lang['mail_filter_banned_file_extension_policy'] = 'Забранени политика файлово разширение';
+$lang['mail_filter_discard_policy'] = 'Отхвърляне политика';
+$lang['mail_filter_file_extension'] = 'Разширение на файл';
+$lang['mail_filter_mail_volume'] = 'Mail том';
+$lang['mail_filter_quarantine'] = 'карантина';
+$lang['mail_filter_quarantine_policy'] = 'Карантина политика';
+$lang['mail_filter_subject_tag'] = 'Тема Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'Тема маркер е невалиден.';
+$lang['mail_filter_virus_detected_policy'] = 'Установена политика Virus';
