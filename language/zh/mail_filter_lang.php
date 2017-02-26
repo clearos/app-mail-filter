aac2f2d120207e479bff5bc1ac1fb03e7c839207
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:06:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = '邮件过滤器引擎提供过滤入站和出站邮件的一套核心工具。';
+$lang['mail_filter_app_name'] = '邮件过滤引擎';
+$lang['mail_filter_bad_header_policy'] = '坏头政策';
+$lang['mail_filter_banned_file_extensions'] = '被禁止的文件扩展名';
+$lang['mail_filter_banned_file_extension_policy'] = '被禁止的文件扩展策略';
+$lang['mail_filter_discard_policy'] = '丢弃策略';
+$lang['mail_filter_file_extension'] = '文件扩展名';
+$lang['mail_filter_mail_volume'] = '邮件量';
+$lang['mail_filter_quarantine'] = '隔离';
+$lang['mail_filter_quarantine_policy'] = '隔离策略';
+$lang['mail_filter_subject_tag'] = '主题标签';
+$lang['mail_filter_subject_tag_invalid'] = '专题标签无效。';
+$lang['mail_filter_virus_detected_policy'] = '检测到的病毒策略';
