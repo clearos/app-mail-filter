ac5565b0720aa84a89e84ffb9d1ece0a8ea5fedb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:13:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sk/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Mail Filter Engine poskytuje základnú sadu nástrojov pre filtrovanie prichádzajúce a odchádzajúce e-mailových správ.';
+$lang['mail_filter_app_name'] = 'Mail Filter Engine';
+$lang['mail_filter_bad_header_policy'] = 'Zásady Bad Header';
+$lang['mail_filter_banned_file_extensions'] = 'Zakázané prípony súborov';
+$lang['mail_filter_banned_file_extension_policy'] = 'Zakázané rozšírenie Policy File';
+$lang['mail_filter_discard_policy'] = 'zrušiť Policy';
+$lang['mail_filter_file_extension'] = 'prípona súboru';
+$lang['mail_filter_mail_volume'] = 'mail Volume';
+$lang['mail_filter_quarantine'] = 'karanténa';
+$lang['mail_filter_quarantine_policy'] = 'Quarantine Policy';
+$lang['mail_filter_subject_tag'] = 'téma Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'Téma tag je neplatný.';
+$lang['mail_filter_virus_detected_policy'] = 'Zistený vírus Policy';
