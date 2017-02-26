033128e1458c73f56c4a49a74435913b5574fae5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:10:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'ಮೇಲ್ ಫಿಲ್ಟರ್ ಎಂಜಿನ್ ಒಳಬರುವ ಮತ್ತು ಹೊರಹೋಗುವ ಮೇಲ್ ಸಂದೇಶಗಳನ್ನು ಶುದ್ಧೀಕರಿಸಲು ಸಾಧನಗಳನ್ನು ಕೋರ್ ಒದಗಿಸುತ್ತದೆ.';
+$lang['mail_filter_app_name'] = 'ಮೇಲ್ ಫಿಲ್ಟರ್ ಎಂಜಿನ್';
+$lang['mail_filter_bad_header_policy'] = 'ಕೆಟ್ಟ ಶಿರೋಲೇಖ ನೀತಿ';
+$lang['mail_filter_banned_file_extensions'] = 'ನಿಷೇಧಿಸಲಾಗಿದೆ ಫೈಲ್ ವಿಸ್ತರಣೆಗಳು';
+$lang['mail_filter_banned_file_extension_policy'] = 'ನಿಷೇಧಿಸಲಾಗಿದೆ ಕಡತ ವಿಸ್ತರಣೆ ನೀತಿ';
+$lang['mail_filter_discard_policy'] = 'ನೀತಿ ತ್ಯಜಿಸು';
+$lang['mail_filter_file_extension'] = 'ಕಡತ ವಿಸ್ತರಣೆ';
+$lang['mail_filter_mail_volume'] = 'ಮೇಲ್ ಸಂಪುಟ';
+$lang['mail_filter_quarantine'] = 'ಸಂಪರ್ಕತಡೆಯನ್ನು';
+$lang['mail_filter_quarantine_policy'] = 'ಮೂಲೆಗುಂಪು ನೀತಿ';
+$lang['mail_filter_subject_tag'] = 'ವಿಷಯ ಟ್ಯಾಗ್';
+$lang['mail_filter_subject_tag_invalid'] = 'ವಿಷಯ ಟ್ಯಾಗ್ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['mail_filter_virus_detected_policy'] = 'ಪತ್ತೆ ವೈರಸ್ ನೀತಿ';
