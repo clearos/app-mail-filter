235a14bd786dd65d2e146bffa9dc34a435bd0e85
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:05:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/be/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Паштовы фільтр рухавіка забяспечвае базавы набор інструментаў для фільтрацыі ўваходзяць і выходных паштовых паведамленняў.';
+$lang['mail_filter_app_name'] = 'Паштовы фільтр рухавіка';
+$lang['mail_filter_bad_header_policy'] = 'Дрэнная палітыка загалоўка';
+$lang['mail_filter_banned_file_extensions'] = 'Забароненыя пашырэння файлаў';
+$lang['mail_filter_banned_file_extension_policy'] = 'Banned Палітыка Пашырэнне файла';
+$lang['mail_filter_discard_policy'] = 'адкінуць палітыка';
+$lang['mail_filter_file_extension'] = 'пашырэнне файла';
+$lang['mail_filter_mail_volume'] = 'аб&#39;ём пошты';
+$lang['mail_filter_quarantine'] = 'каранцінны';
+$lang['mail_filter_quarantine_policy'] = 'каранцін палітыкі';
+$lang['mail_filter_subject_tag'] = 'тэма тэгаў';
+$lang['mail_filter_subject_tag_invalid'] = 'Тэма тэг з&#39;яўляецца несапраўдным.';
+$lang['mail_filter_virus_detected_policy'] = 'Знойдзены палітыкі Вірус';
