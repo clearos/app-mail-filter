4a598b3c5c88752d044911eafd23272df02bdb47
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:10:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Vivamus Mail The Filter Set suggero tools ut Core a filtering inbound et outbound mail nuntius.';
+$lang['mail_filter_app_name'] = 'Vivamus Mail Filter';
+$lang['mail_filter_bad_header_policy'] = 'Header malum Policy';
+$lang['mail_filter_banned_file_extensions'] = 'File Vestibulum Avatar';
+$lang['mail_filter_banned_file_extension_policy'] = 'Forum Extension File Policy';
+$lang['mail_filter_discard_policy'] = 'vocares Policy';
+$lang['mail_filter_file_extension'] = 'lima Tractus';
+$lang['mail_filter_mail_volume'] = 'Volume Mail';
+$lang['mail_filter_quarantine'] = 'LOGINQUITAS';
+$lang['mail_filter_quarantine_policy'] = 'LOGINQUITAS Policy';
+$lang['mail_filter_subject_tag'] = 'Tag Subject';
+$lang['mail_filter_subject_tag_invalid'] = 'Tag Subject est invalidum.';
+$lang['mail_filter_virus_detected_policy'] = 'Results Viris Policy';
