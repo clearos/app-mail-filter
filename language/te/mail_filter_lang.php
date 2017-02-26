adda74f057a81358157f6c3d1a2e16dcc4e4957e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:15:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'మెయిల్ వడపోత ఇంజిన్ రవాణాల్లో మెయిల్ సందేశాలను వడపోత టూల్స్ యొక్క కోర్ అమరుస్తుంది.';
+$lang['mail_filter_app_name'] = 'మెయిల్ వడపోత ఇంజిన్';
+$lang['mail_filter_bad_header_policy'] = 'బాడ్ శీర్షిక విధానం';
+$lang['mail_filter_banned_file_extensions'] = 'నిషేధింపబడిన ఫైల్ పొడిగింపులను';
+$lang['mail_filter_banned_file_extension_policy'] = 'నిషేధింపబడిన ఫైల్ పొడిగింపు విధానం';
+$lang['mail_filter_discard_policy'] = 'విస్మ విధానం';
+$lang['mail_filter_file_extension'] = 'ఫైల్ పొడిగింపు';
+$lang['mail_filter_mail_volume'] = 'మెయిల్ వాల్యూమ్';
+$lang['mail_filter_quarantine'] = 'దిగ్బంధానికి';
+$lang['mail_filter_quarantine_policy'] = 'దిగ్బంధానికి విధానం';
+$lang['mail_filter_subject_tag'] = 'విషయం ట్యాగ్';
+$lang['mail_filter_subject_tag_invalid'] = 'విషయం ట్యాగ్ చెల్లదు.';
+$lang['mail_filter_virus_detected_policy'] = 'దొరికింది వైరస్ విధానం';
