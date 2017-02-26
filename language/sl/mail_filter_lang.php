9ab281e9d4e74db85ccac39dda4b439aa4787717
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:13:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Mail Filter Motor zagotavlja osnovno nabor orodij za filtriranje vhodnih in izhodnih poštnih sporočil.';
+$lang['mail_filter_app_name'] = 'Mail Filter motorja';
+$lang['mail_filter_bad_header_policy'] = 'Bad politika Glava';
+$lang['mail_filter_banned_file_extensions'] = 'Prepovedani končnic datotek';
+$lang['mail_filter_banned_file_extension_policy'] = 'Prepovedani datoteke Razširitev politike';
+$lang['mail_filter_discard_policy'] = 'Zavrzi politika';
+$lang['mail_filter_file_extension'] = 'Razširitev datoteke';
+$lang['mail_filter_mail_volume'] = 'mail Volume';
+$lang['mail_filter_quarantine'] = 'Karantena';
+$lang['mail_filter_quarantine_policy'] = 'Karantena politika';
+$lang['mail_filter_subject_tag'] = 'predmet Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'Ob tag je neveljavna.';
+$lang['mail_filter_virus_detected_policy'] = 'Zaznana je politika Virus';
