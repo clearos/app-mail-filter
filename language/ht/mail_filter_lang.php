2e7315c2123fa04143c52b4c4ec661cd6f26bf9a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:08:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Motè a Mail Filter bay yon seri debaz nan zouti pou filtraj mail mesaj Anthrax ak sortant.';
+$lang['mail_filter_app_name'] = 'Mail Filter Engine';
+$lang['mail_filter_bad_header_policy'] = 'Move Policy Header';
+$lang['mail_filter_banned_file_extensions'] = 'Entèdi File ekstansyon';
+$lang['mail_filter_banned_file_extension_policy'] = 'Entèdi Policy Ekstansyon File';
+$lang['mail_filter_discard_policy'] = 'Jete Policy';
+$lang['mail_filter_file_extension'] = 'file Ekstansyon';
+$lang['mail_filter_mail_volume'] = 'Volim mail';
+$lang['mail_filter_quarantine'] = 'karantèn';
+$lang['mail_filter_quarantine_policy'] = 'karantèn Règleman';
+$lang['mail_filter_subject_tag'] = 'sijè Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'Sijè tag se valab.';
+$lang['mail_filter_virus_detected_policy'] = 'Detected Policy Virus';
