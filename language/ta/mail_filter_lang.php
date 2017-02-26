0b30ea85dcb071897a1e30fe8f3b0223ae5ad566
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:15:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'மின்னஞ்சல் வடிகட்டி பொறி உள்ளூர் மற்றும் வெளிநாட்டு சுற்றுலா மெயில் தகவல்களை வடிகட்ட பயன்படும் கருவிகள் ஒரு முக்கிய தொகுப்பு வழங்குகிறது.';
+$lang['mail_filter_app_name'] = 'மின்னஞ்சல் வடிகட்டி பொறி';
+$lang['mail_filter_bad_header_policy'] = 'பேட் தலைப்பு கொள்கை';
+$lang['mail_filter_banned_file_extensions'] = 'தடை செய்யப்பட்ட கோப்பு நீட்டிப்புகள்';
+$lang['mail_filter_banned_file_extension_policy'] = 'தடை செய்யப்பட்ட கோப்பு நீட்டிப்பு கொள்கை';
+$lang['mail_filter_discard_policy'] = 'நிராகரி கொள்கை';
+$lang['mail_filter_file_extension'] = 'கோப்பு நீட்டிப்பு';
+$lang['mail_filter_mail_volume'] = 'மின்னஞ்சல் தொகுதி';
+$lang['mail_filter_quarantine'] = 'தொற்றுநோய்';
+$lang['mail_filter_quarantine_policy'] = 'தொற்றுநோய் கொள்கை';
+$lang['mail_filter_subject_tag'] = 'பொருள் டேக்';
+$lang['mail_filter_subject_tag_invalid'] = 'பொருள் டேக் தவறானது.';
+$lang['mail_filter_virus_detected_policy'] = 'கண்டறியப்பட்டது வைரஸ் கொள்கை';
