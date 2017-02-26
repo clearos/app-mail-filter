3dbba7a271409a7298ec63179c900f75ab883bfd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:09:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Soláthraíonn an Inneall Mail Scagaire sraith lárnach na n-uirlisí chun scagadh teachtaireachtaí ríomhphoist isteach agus amach.';
+$lang['mail_filter_app_name'] = 'Inneall Scagaire Mail';
+$lang['mail_filter_bad_header_policy'] = 'Polasaí Ceanntásc dona';
+$lang['mail_filter_banned_file_extensions'] = 'Eisínteachtaí Comhad Banned';
+$lang['mail_filter_banned_file_extension_policy'] = 'Banned Polasaí Síneadh Comhad';
+$lang['mail_filter_discard_policy'] = 'Polasaí Ná Sábháil';
+$lang['mail_filter_file_extension'] = 'Síneadh comhad';
+$lang['mail_filter_mail_volume'] = 'Mail Imleabhar';
+$lang['mail_filter_quarantine'] = 'coraintín';
+$lang['mail_filter_quarantine_policy'] = 'Polasaí coraintín';
+$lang['mail_filter_subject_tag'] = 'Clib ábhar';
+$lang['mail_filter_subject_tag_invalid'] = 'Is chlib Ábhar neamhbhailí.';
+$lang['mail_filter_virus_detected_policy'] = 'Polasaí Víreas Braite';
