419a1a44a742633a00c3bcd1670b2aafb1a666df
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:09:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'メールフィルタエンジンは、インバウンドとアウトバウンドのメールメッセージをフィルタリングするためのツールのコアセットを提供します。';
+$lang['mail_filter_app_name'] = 'メールフィルタエンジン';
+$lang['mail_filter_bad_header_policy'] = '悪いヘッダーポリシー';
+$lang['mail_filter_banned_file_extensions'] = '禁止ファイル拡張子';
+$lang['mail_filter_banned_file_extension_policy'] = '禁止ファイル拡張子方針';
+$lang['mail_filter_discard_policy'] = '廃棄ポリシー';
+$lang['mail_filter_file_extension'] = 'ファイル拡張子';
+$lang['mail_filter_mail_volume'] = '郵便量';
+$lang['mail_filter_quarantine'] = '検疫';
+$lang['mail_filter_quarantine_policy'] = '隔離政策';
+$lang['mail_filter_subject_tag'] = '件名タグ';
+$lang['mail_filter_subject_tag_invalid'] = '件名タグが無効です。';
+$lang['mail_filter_virus_detected_policy'] = '検出されたウイルスポリシー';
