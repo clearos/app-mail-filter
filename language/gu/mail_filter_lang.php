df2f2c64edb9d2ee5e7bea4df13aac3d071413f1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:08:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'મેલ ફિલ્ટર એન્જિન ઈનબાઉન્ડ અને આઉટબાઉન્ડ મેલ મેસેજીસ ગાળણ માટે સાધનો કોર સમૂહ પૂરો પાડે છે.';
+$lang['mail_filter_app_name'] = 'મેલ ફિલ્ટર એન્જિન';
+$lang['mail_filter_bad_header_policy'] = 'ખરાબ મથાળું નીતિ';
+$lang['mail_filter_banned_file_extensions'] = 'પર પ્રતિબંધ મૂક્યો ફાઇલ એક્સ્ટેન્શન્સ';
+$lang['mail_filter_banned_file_extension_policy'] = 'પર પ્રતિબંધ મૂક્યો ફાઇલ એક્સ્ટેંશન નીતિ';
+$lang['mail_filter_discard_policy'] = 'કાઢી નાખો નીતિ';
+$lang['mail_filter_file_extension'] = 'ફાઇલ એક્સ્ટેંશન';
+$lang['mail_filter_mail_volume'] = 'મેલ વોલ્યુમ';
+$lang['mail_filter_quarantine'] = 'સંસર્ગનિષેધ';
+$lang['mail_filter_quarantine_policy'] = 'સંસર્ગનિષેધ નીતિ';
+$lang['mail_filter_subject_tag'] = 'વિષય ટેગ';
+$lang['mail_filter_subject_tag_invalid'] = 'વિષય ટેગ અમાન્ય છે.';
+$lang['mail_filter_virus_detected_policy'] = 'શોધાયેલ વાયરસ નીતિ';
