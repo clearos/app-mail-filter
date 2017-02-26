6867337bf9cb995216098270273c5b3f60b52b12
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:05:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'အဆိုပါမေးလ် Filter ကိုအင်ဂျင်ဘက်ကနှင့်အပြင်ထွက်မေးလ်မက်ဆေ့ခ်ျစီစစ်ဘို့ tools တွေကိုတစ်ဦး core ကို set ကိုပေးစွမ်းသည်။';
+$lang['mail_filter_app_name'] = 'မေးလ် Filter ကိုအင်ဂျင်ပါဝါ';
+$lang['mail_filter_bad_header_policy'] = 'မကောင်းတဲ့ Header ကိုပေါ်လစီ';
+$lang['mail_filter_banned_file_extensions'] = 'ပိတ်ပင်ခဲ့ဖိုင် Extensions';
+$lang['mail_filter_banned_file_extension_policy'] = 'ပိတ်ပင်ခဲ့ဖိုင်တိုးချဲ့မှုပေါ်လစီ';
+$lang['mail_filter_discard_policy'] = 'ပေါ်လစီဖယ်ထုတ်ပစ်';
+$lang['mail_filter_file_extension'] = 'File ကိုတိုးချဲ့';
+$lang['mail_filter_mail_volume'] = 'မေးလ် Volume ကို';
+$lang['mail_filter_quarantine'] = 'ရောဂါမပျံနှံ့အောင်ကာဆီးတားခြင်း';
+$lang['mail_filter_quarantine_policy'] = 'quarantine ပေါ်လစီ';
+$lang['mail_filter_subject_tag'] = 'ဘာသာရပ် Tag ကို';
+$lang['mail_filter_subject_tag_invalid'] = 'ဘာသာရပ် tag ကိုမမှန်ကန်ဘူး။';
+$lang['mail_filter_virus_detected_policy'] = 'Detected ဗိုင်းရပ်စ်ပေါ်လစီ';
