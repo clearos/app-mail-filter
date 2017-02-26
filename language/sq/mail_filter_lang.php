737443cacb993e9781a863403d5edd1c656e2cca
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:04:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Engine Mail Filter ofron një sërë bazë të mjeteve për filtrimin e mesazheve përbrenda dhe jashtë postës.';
+$lang['mail_filter_app_name'] = 'Mail Filter Engine';
+$lang['mail_filter_bad_header_policy'] = 'Header Politika Bad';
+$lang['mail_filter_banned_file_extensions'] = 'Zgjerime Banned dokumentin';
+$lang['mail_filter_banned_file_extension_policy'] = 'Banned dokumentit Politika Extension';
+$lang['mail_filter_discard_policy'] = 'Politika Discard';
+$lang['mail_filter_file_extension'] = 'File Extension';
+$lang['mail_filter_mail_volume'] = 'Mail Volume';
+$lang['mail_filter_quarantine'] = 'karantinë';
+$lang['mail_filter_quarantine_policy'] = 'Politika Quarantine';
+$lang['mail_filter_subject_tag'] = 'Subject Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'tag Subjekti është i pavlefshëm.';
+$lang['mail_filter_virus_detected_policy'] = 'Zbuluar Virus Policy';
