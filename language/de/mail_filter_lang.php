3e456b17ec600374ab7c226500740cea139661e5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:07:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/de/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Die Mail-Filter-Engine stellt einen Kernsatz von Tools zum Filtern ein- und ausgehenden Mail-Nachrichten.';
+$lang['mail_filter_app_name'] = 'Mail-Filter-Engine';
+$lang['mail_filter_bad_header_policy'] = 'Bad-Header-Politik';
+$lang['mail_filter_banned_file_extensions'] = 'Gebannt Dateierweiterungen';
+$lang['mail_filter_banned_file_extension_policy'] = 'Gebannt Dateierweiterung Politik';
+$lang['mail_filter_discard_policy'] = 'Discard-Politik';
+$lang['mail_filter_file_extension'] = 'Dateierweiterung';
+$lang['mail_filter_mail_volume'] = 'Mail-Volume';
+$lang['mail_filter_quarantine'] = 'Quarantäne';
+$lang['mail_filter_quarantine_policy'] = 'Quarantäne-Politik';
+$lang['mail_filter_subject_tag'] = 'Betreff Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'Betreff-Kennzeichen ist ungültig.';
+$lang['mail_filter_virus_detected_policy'] = 'Erkannt Virus Politik';
