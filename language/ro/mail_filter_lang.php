e3a9b6d33136f08bd46ac47154d351fde0658010
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:13:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Mail Filter Engine ofera un set de bază de instrumente pentru filtrarea mesajelor e-mail de intrare și de ieșire.';
+$lang['mail_filter_app_name'] = 'Filtru e-mail Motor';
+$lang['mail_filter_bad_header_policy'] = 'Politica Antet rea';
+$lang['mail_filter_banned_file_extensions'] = 'Banned extensiile de fișiere';
+$lang['mail_filter_banned_file_extension_policy'] = 'Banat Politica de extensie de fișier';
+$lang['mail_filter_discard_policy'] = 'Politica se debarasa';
+$lang['mail_filter_file_extension'] = 'Extensie fișier';
+$lang['mail_filter_mail_volume'] = 'Volumul e-mail';
+$lang['mail_filter_quarantine'] = 'Carantină';
+$lang['mail_filter_quarantine_policy'] = 'Politica de carantină';
+$lang['mail_filter_subject_tag'] = 'Etichetă de subiect';
+$lang['mail_filter_subject_tag_invalid'] = 'Eticheta de subiect este nevalid.';
+$lang['mail_filter_virus_detected_policy'] = 'Detectate Politica Virus';
