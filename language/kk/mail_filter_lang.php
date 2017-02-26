933f57804d51f08ca26dbe3e26506f246d6bd192
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:10:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Пошта сүзгісі Қозғалтқыш кіріс және шығыс пошта хабарларын сүзу құралдарының негізгі жиынтығын ұсынады.';
+$lang['mail_filter_app_name'] = 'Пошта сүзгісі Қозғалтқыш';
+$lang['mail_filter_bad_header_policy'] = 'Bad Header саясаты';
+$lang['mail_filter_banned_file_extensions'] = 'Тыйым салынған файл кеңейтімдері';
+$lang['mail_filter_banned_file_extension_policy'] = 'Тыйым салынған Файл кеңейтімі саясаты';
+$lang['mail_filter_discard_policy'] = 'саясатын тастаңыз';
+$lang['mail_filter_file_extension'] = 'файл кеңейтімі';
+$lang['mail_filter_mail_volume'] = 'Пошта көлемі';
+$lang['mail_filter_quarantine'] = 'карантин';
+$lang['mail_filter_quarantine_policy'] = 'Карантин саясаты';
+$lang['mail_filter_subject_tag'] = 'Тақырыбы Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'Тақырыбы тег жарамсыз болып табылады.';
+$lang['mail_filter_virus_detected_policy'] = 'Анықталған Вирус саясаты';
