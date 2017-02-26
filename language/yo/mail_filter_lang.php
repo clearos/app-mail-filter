d8bac6c7a2c087d03d60840d6d2471883d83c547
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:16:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'The Mail Filter Engine pese a mojuto ṣeto ti irinṣẹ fun sisẹ inbound ati ti o njade mail awọn ifiranṣẹ.';
+$lang['mail_filter_app_name'] = 'Mail Filter Engine';
+$lang['mail_filter_bad_header_policy'] = 'Bad akọsori Afihan';
+$lang['mail_filter_banned_file_extensions'] = 'Gbesele File amugbooro';
+$lang['mail_filter_banned_file_extension_policy'] = 'Gbesele File Itẹsiwaju Afihan';
+$lang['mail_filter_discard_policy'] = 'àyípadà Policy';
+$lang['mail_filter_file_extension'] = 'faili Itẹsiwaju';
+$lang['mail_filter_mail_volume'] = 'mail didun';
+$lang['mail_filter_quarantine'] = 'quarantine';
+$lang['mail_filter_quarantine_policy'] = 'quarantine Afihan';
+$lang['mail_filter_subject_tag'] = 'koko Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'Koko tag ni invalid.';
+$lang['mail_filter_virus_detected_policy'] = '-Ri Iwoye Afihan';
