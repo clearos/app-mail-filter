cebf2e069363dca25c35dee53e2154e42453675e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:13:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'පෙරණයක් එන්ජින් අභ්යන්තර යොමුගත වනදාට තැපැල් ලිපි පෙරණය සඳහා මෙවලම් මූලික මාලාවක් සපයයි.';
+$lang['mail_filter_app_name'] = 'පෙරණයක් එන්ජින්';
+$lang['mail_filter_bad_header_policy'] = 'නරක ශීර්ෂකය ප්රතිපත්ති';
+$lang['mail_filter_banned_file_extensions'] = 'තහනම් ගොනුව දිගු';
+$lang['mail_filter_banned_file_extension_policy'] = 'තහනම් ගොනුව ව්යාප්ති ප්රතිපත්ති';
+$lang['mail_filter_discard_policy'] = 'ප්රතිපත්ති ඉවත ලන්න';
+$lang['mail_filter_file_extension'] = 'ගොනුව ව්යාප්ති';
+$lang['mail_filter_mail_volume'] = 'තැපැල් වෙළුම';
+$lang['mail_filter_quarantine'] = 'නිරෝධායන';
+$lang['mail_filter_quarantine_policy'] = 'නිරෝධායන ප්රතිපත්ති';
+$lang['mail_filter_subject_tag'] = 'විෂය ටැග';
+$lang['mail_filter_subject_tag_invalid'] = 'විෂය ටැගය වලංගු නැත.';
+$lang['mail_filter_virus_detected_policy'] = 'අනාවරණය වයිරස් ප්රතිපත්ති';
