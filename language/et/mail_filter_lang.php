8dbc774a43993278882fba6aecc0efe5943b4aaf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:07:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Mail Filter Mootor annab core tööriistade komplekt filtreerimine sissetulevate ja väljaminevate kirjade.';
+$lang['mail_filter_app_name'] = 'Mail Filter Mootor';
+$lang['mail_filter_bad_header_policy'] = 'Bad Päise Policy';
+$lang['mail_filter_banned_file_extensions'] = 'Keelatud faililaiendite';
+$lang['mail_filter_banned_file_extension_policy'] = 'Keelatud File Extension Policy';
+$lang['mail_filter_discard_policy'] = 'Loobu Policy';
+$lang['mail_filter_file_extension'] = 'file Extension';
+$lang['mail_filter_mail_volume'] = 'mail Volume';
+$lang['mail_filter_quarantine'] = 'karantiin';
+$lang['mail_filter_quarantine_policy'] = 'karantiinireeglid';
+$lang['mail_filter_subject_tag'] = 'Teema Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'Teema silt on vigane.';
+$lang['mail_filter_virus_detected_policy'] = 'Avastati Virus Policy';
