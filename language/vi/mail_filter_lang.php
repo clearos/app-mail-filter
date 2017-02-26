94ebbe8a7f628481cc0e87373b9d21819f867485
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:16:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Engine Thư Lọc cung cấp một bộ lõi của các công cụ để lọc mail trong và ngoài nước.';
+$lang['mail_filter_app_name'] = 'Thư Lọc động cơ';
+$lang['mail_filter_bad_header_policy'] = 'Chính sách Tiêu đề Bad';
+$lang['mail_filter_banned_file_extensions'] = 'Mở rộng File Banned';
+$lang['mail_filter_banned_file_extension_policy'] = 'Banned Chính sách mở rộng tập tin';
+$lang['mail_filter_discard_policy'] = 'Chính sách Huỷ';
+$lang['mail_filter_file_extension'] = 'file Extension';
+$lang['mail_filter_mail_volume'] = 'thư Tập';
+$lang['mail_filter_quarantine'] = 'kiểm dịch';
+$lang['mail_filter_quarantine_policy'] = 'Chính sách kiểm dịch';
+$lang['mail_filter_subject_tag'] = 'Tag Chủ đề';
+$lang['mail_filter_subject_tag_invalid'] = 'thẻ tiêu đề là không hợp lệ.';
+$lang['mail_filter_virus_detected_policy'] = 'Chính sách virus được phát hiện';
