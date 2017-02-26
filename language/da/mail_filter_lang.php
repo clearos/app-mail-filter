80627b067267fb017a806babb16b77dd165d9316
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:06:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Mail Filter Engine giver en kerne af værktøjer til filtrering indgående og udgående mails.';
+$lang['mail_filter_app_name'] = 'Mail Filter Engine';
+$lang['mail_filter_bad_header_policy'] = 'Dårlig politik Header';
+$lang['mail_filter_banned_file_extensions'] = 'Banned filtyper';
+$lang['mail_filter_banned_file_extension_policy'] = 'Banned filtypenavn Politik';
+$lang['mail_filter_discard_policy'] = 'Kassér Politik';
+$lang['mail_filter_file_extension'] = 'File Extension';
+$lang['mail_filter_mail_volume'] = 'Mail Volume';
+$lang['mail_filter_quarantine'] = 'Karantæne';
+$lang['mail_filter_quarantine_policy'] = 'Karantæne Politik';
+$lang['mail_filter_subject_tag'] = 'Emne Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'Emne tag er ugyldig.';
+$lang['mail_filter_virus_detected_policy'] = 'Opdagede Virus Politik';
