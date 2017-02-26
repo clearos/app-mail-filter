7b58ee1d4e93b6165bf6e2bdca40ab02a4453a51
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:05:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Mail Filter Engine The core tresna multzo bat mezu mail sarrerako eta irteerako iragazteko eskaintzen.';
+$lang['mail_filter_app_name'] = 'Mail Filter Engine';
+$lang['mail_filter_bad_header_policy'] = 'Bad Goiburuko Politika';
+$lang['mail_filter_banned_file_extensions'] = 'Debekatutako fitxategi luzapenak';
+$lang['mail_filter_banned_file_extension_policy'] = 'Debekatuta File Extension Policy';
+$lang['mail_filter_discard_policy'] = 'Baztertu politika';
+$lang['mail_filter_file_extension'] = 'Fitxategiaren luzapena';
+$lang['mail_filter_mail_volume'] = 'Mail Bolumen';
+$lang['mail_filter_quarantine'] = 'Koarentena';
+$lang['mail_filter_quarantine_policy'] = 'Koarentena politika';
+$lang['mail_filter_subject_tag'] = 'Gaia Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'Gaia etiketa baliogabea da.';
+$lang['mail_filter_virus_detected_policy'] = 'Detektatutako Virus Politika';
