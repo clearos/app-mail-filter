57a24e4fc90fba3e21b63b0eb3b4ed0d209d19a1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:06:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'La Poŝto Filtrilo Engine provizas kernan aro de iloj por filtri inbound kaj outbound retmesagxojn.';
+$lang['mail_filter_app_name'] = 'Poŝto Filtrilo Motoro';
+$lang['mail_filter_bad_header_policy'] = 'Malbona Header Politiko';
+$lang['mail_filter_banned_file_extensions'] = 'Malpermesita Dosiero Vastigado';
+$lang['mail_filter_banned_file_extension_policy'] = 'Malpermesita Dosiero Pligrandigo Politiko';
+$lang['mail_filter_discard_policy'] = 'Forĵeti Politiko';
+$lang['mail_filter_file_extension'] = 'dosiero Pligrandigo';
+$lang['mail_filter_mail_volume'] = 'poŝto Volumo';
+$lang['mail_filter_quarantine'] = 'kvaranteno';
+$lang['mail_filter_quarantine_policy'] = 'kvaranteno Politiko';
+$lang['mail_filter_subject_tag'] = 'temo Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'Temo etikedo estas nevalida.';
+$lang['mail_filter_virus_detected_policy'] = 'Detektitaj Virus Politiko';
