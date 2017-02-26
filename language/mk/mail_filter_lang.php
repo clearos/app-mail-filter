d96722142d0f352c8bc3d55a8673b4a0679831b9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:11:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Мотор пошта филтер обезбедува основен сет на алатки за филтрирање Влезни и излезни mail пораки.';
+$lang['mail_filter_app_name'] = 'Пошта филтер на моторот';
+$lang['mail_filter_bad_header_policy'] = 'Лоша политика заглавие';
+$lang['mail_filter_banned_file_extensions'] = 'Забранет File Extensions';
+$lang['mail_filter_banned_file_extension_policy'] = 'Забранет политика наставката на датотеката';
+$lang['mail_filter_discard_policy'] = 'Отфрли политика';
+$lang['mail_filter_file_extension'] = 'наставката на датотеката';
+$lang['mail_filter_mail_volume'] = 'пошта Том';
+$lang['mail_filter_quarantine'] = 'карантин';
+$lang['mail_filter_quarantine_policy'] = 'карантин политика';
+$lang['mail_filter_subject_tag'] = 'предмет Таг';
+$lang['mail_filter_subject_tag_invalid'] = 'Предмет таг не е валиден.';
+$lang['mail_filter_virus_detected_policy'] = 'Откриен вирус Политика';
