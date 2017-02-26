3bbdaa793114659baeb6d2dd1ea49f092a39244a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:04:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Mail Filter Engine gelen ve giden poçt mesajları filtreleme üçün alətlər əsas təmin edir.';
+$lang['mail_filter_app_name'] = 'Mail Filter Engine';
+$lang['mail_filter_bad_header_policy'] = 'Bad Header Siyasət';
+$lang['mail_filter_banned_file_extensions'] = 'Qadağan File Extensions';
+$lang['mail_filter_banned_file_extension_policy'] = 'Qadağan File Extension Policy';
+$lang['mail_filter_discard_policy'] = 'Sil Siyasət';
+$lang['mail_filter_file_extension'] = 'File Extension';
+$lang['mail_filter_mail_volume'] = 'Mail Volume';
+$lang['mail_filter_quarantine'] = 'karantin';
+$lang['mail_filter_quarantine_policy'] = 'Karantin Siyasət';
+$lang['mail_filter_subject_tag'] = 'Mövzu Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'Mövzu tag yanlışdır.';
+$lang['mail_filter_virus_detected_policy'] = 'Aşkar Virus Siyasət';
