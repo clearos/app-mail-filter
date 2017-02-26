efa6d5ffcb4964441b55e87d03da401f9b0b54a1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:14:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Barua Filter injini za hutoa msingi seti ya zana kwa ajili ya kuchuja ujumbe inbound na utlandet pepe.';
+$lang['mail_filter_app_name'] = 'Barua Filter injini za';
+$lang['mail_filter_bad_header_policy'] = 'Bad Sera Header';
+$lang['mail_filter_banned_file_extensions'] = 'Marufuku faili upanuzi';
+$lang['mail_filter_banned_file_extension_policy'] = 'Marufuku faili Sera ugani';
+$lang['mail_filter_discard_policy'] = 'Tupa Sera';
+$lang['mail_filter_file_extension'] = 'Picha ugani';
+$lang['mail_filter_mail_volume'] = 'Barua Volume';
+$lang['mail_filter_quarantine'] = 'Quarantine';
+$lang['mail_filter_quarantine_policy'] = 'Quarantine Sera';
+$lang['mail_filter_subject_tag'] = 'Subject Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'Subject tag ni batili.';
+$lang['mail_filter_virus_detected_policy'] = 'Wanaona Virus Sera';
