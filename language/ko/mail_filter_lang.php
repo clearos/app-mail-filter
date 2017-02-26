f246a70dc1d6f6a357fa32827bf8d4de5794efbf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:10:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = '메일 필터 엔진은 인바운드 및 아웃 바운드 메일 메시지를 필터링하기위한 도구의 핵심 세트를 제공합니다.';
+$lang['mail_filter_app_name'] = '메일 필터 엔진';
+$lang['mail_filter_bad_header_policy'] = '나쁜 헤더 정책';
+$lang['mail_filter_banned_file_extensions'] = '금지 된 파일 확장명';
+$lang['mail_filter_banned_file_extension_policy'] = '금지 된 파일 확장자 정책';
+$lang['mail_filter_discard_policy'] = '취소 정책';
+$lang['mail_filter_file_extension'] = '파일 확장자';
+$lang['mail_filter_mail_volume'] = '메일 볼륨';
+$lang['mail_filter_quarantine'] = '격리';
+$lang['mail_filter_quarantine_policy'] = '검역 정책';
+$lang['mail_filter_subject_tag'] = '주제 태그';
+$lang['mail_filter_subject_tag_invalid'] = '주제 태그가 잘못되었습니다.';
+$lang['mail_filter_virus_detected_policy'] = '탐지 된 바이러스 정책';
