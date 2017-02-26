53d8e1c487bf2ea0e33f26dec6ebb64337fcfa75
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:04:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Փոստ Filter Շարժիչի ապահովում առանցքային շարք գործիքների զտիչ ներգնա եւ ուղեվորվող Mail հաղորդագրությունները.';
+$lang['mail_filter_app_name'] = 'Փոստ Filter Engine';
+$lang['mail_filter_bad_header_policy'] = 'Bad Վերնագրի քաղաքականություն';
+$lang['mail_filter_banned_file_extensions'] = 'Արգելափակված ֆայլի ընդարձակման';
+$lang['mail_filter_banned_file_extension_policy'] = 'Արգելվել File Extension քաղաքականություն';
+$lang['mail_filter_discard_policy'] = 'Վերացնել քաղաքականություն';
+$lang['mail_filter_file_extension'] = 'ֆայլի ընդլայնում';
+$lang['mail_filter_mail_volume'] = 'Փոստ Volume';
+$lang['mail_filter_quarantine'] = 'կարանտին';
+$lang['mail_filter_quarantine_policy'] = 'կարանտինի քաղաքականություն';
+$lang['mail_filter_subject_tag'] = 'Թեմա Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'Թեման կտրոնը է անվավեր:';
+$lang['mail_filter_virus_detected_policy'] = 'Հայտնաբերվում Virus քաղաքականություն';
