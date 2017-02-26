f6deeefe07cacc1c7f43b0a589893b5a9abd7628
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:12:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'De Mail Filter Engine biedt een kern set van tools voor het filteren van inkomende en uitgaande e-mailberichten.';
+$lang['mail_filter_app_name'] = 'Mail Filter Engine';
+$lang['mail_filter_bad_header_policy'] = 'Bad Header Beleid';
+$lang['mail_filter_banned_file_extensions'] = 'Verboden bestandsextensies';
+$lang['mail_filter_banned_file_extension_policy'] = 'Banned bestandsextensie Beleid';
+$lang['mail_filter_discard_policy'] = 'teruggooi beleid';
+$lang['mail_filter_file_extension'] = 'Bestandsextensie';
+$lang['mail_filter_mail_volume'] = 'mail Volume';
+$lang['mail_filter_quarantine'] = 'Quarantaine';
+$lang['mail_filter_quarantine_policy'] = 'quarantaine beleid';
+$lang['mail_filter_subject_tag'] = 'onderwerp Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'Onderwerp tag is ongeldig.';
+$lang['mail_filter_virus_detected_policy'] = 'Gedetecteerd Virus Beleid';
