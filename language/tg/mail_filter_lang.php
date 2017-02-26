f7ca63c3648335a36f2e6727a0bf71aa03ff13bc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:15:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Дар Engine Mail Филтр як маҷмӯи асосии восита барои полоиши паёмҳои почтаи даромад ва содиротии таъмин менамояд.';
+$lang['mail_filter_app_name'] = 'Mail Engine Филтри';
+$lang['mail_filter_bad_header_policy'] = 'Сиёсати сарлавҳаи бад';
+$lang['mail_filter_banned_file_extensions'] = 'Васеъшавии File манъ';
+$lang['mail_filter_banned_file_extension_policy'] = 'Сиёсати Тамдиди File манъ';
+$lang['mail_filter_discard_policy'] = 'сиёсати, меоварад';
+$lang['mail_filter_file_extension'] = 'Тамдиди File';
+$lang['mail_filter_mail_volume'] = 'Mail ҷилди';
+$lang['mail_filter_quarantine'] = 'карантин';
+$lang['mail_filter_quarantine_policy'] = 'сиёсати карантини';
+$lang['mail_filter_subject_tag'] = 'Мавзӯи барчасбҳо';
+$lang['mail_filter_subject_tag_invalid'] = 'Мавзӯъ барчасп нодуруст аст.';
+$lang['mail_filter_virus_detected_policy'] = 'Сиёсати Virus ошкор';
