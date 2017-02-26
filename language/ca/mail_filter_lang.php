4a0e2dab0b7fe1f1dcd1b95cfe626df3cb7f78c7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:06:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'El motor de filtre de correu proporciona un conjunt bàsic d&#39;eines per al filtrat de missatges de correu electrònic entrants i sortints.';
+$lang['mail_filter_app_name'] = 'Motor de filtre de correu';
+$lang['mail_filter_bad_header_policy'] = 'Política de mala Capçalera';
+$lang['mail_filter_banned_file_extensions'] = 'Extensions d&#39;arxiu prohibits';
+$lang['mail_filter_banned_file_extension_policy'] = 'Política extensió d&#39;arxiu prohibit';
+$lang['mail_filter_discard_policy'] = 'política de descart';
+$lang['mail_filter_file_extension'] = 'Extensió del fitxer';
+$lang['mail_filter_mail_volume'] = 'El volum de correu';
+$lang['mail_filter_quarantine'] = 'quarantena';
+$lang['mail_filter_quarantine_policy'] = 'política de quarantena';
+$lang['mail_filter_subject_tag'] = 'assumpte Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'Etiqueta de l&#39;assumpte no és vàlid.';
+$lang['mail_filter_virus_detected_policy'] = 'Política Virus detectat';
