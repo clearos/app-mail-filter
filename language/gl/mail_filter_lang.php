73b5e89edd8bcc1d2c20528b26639a19be0fc0d3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:07:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gl/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'O motor Mail Filter ofrece un conxunto de ferramentas para a filtrado de mensaxes de correo de entrada e saída.';
+$lang['mail_filter_app_name'] = 'Filtro de motor correo';
+$lang['mail_filter_bad_header_policy'] = 'Política Header Bad';
+$lang['mail_filter_banned_file_extensions'] = 'Extensións de arquivos prohibidos';
+$lang['mail_filter_banned_file_extension_policy'] = 'Política de extensión de arquivo prohibidos';
+$lang['mail_filter_discard_policy'] = 'política de descartar';
+$lang['mail_filter_file_extension'] = 'Extensión de arquivo';
+$lang['mail_filter_mail_volume'] = 'Volume de correo';
+$lang['mail_filter_quarantine'] = 'corentena';
+$lang['mail_filter_quarantine_policy'] = 'política de corentena';
+$lang['mail_filter_subject_tag'] = 'tag Tema';
+$lang['mail_filter_subject_tag_invalid'] = 'tag asunto non é válido.';
+$lang['mail_filter_virus_detected_policy'] = 'Política Virus detectado';
