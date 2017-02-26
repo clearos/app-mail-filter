e58fa1229e506f1cf184fc69c2cbfb6592bf1ce0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:08:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'A Mail Filter Engine egy központi eszközök szűrés bejövő és kimenő üzeneteket.';
+$lang['mail_filter_app_name'] = 'Mail Filter Motor';
+$lang['mail_filter_bad_header_policy'] = 'Bad Fejléc Policy';
+$lang['mail_filter_banned_file_extensions'] = 'Betiltották fájlkiterjesztés';
+$lang['mail_filter_banned_file_extension_policy'] = 'Betiltották fájl kiterjesztése Policy';
+$lang['mail_filter_discard_policy'] = 'elvetése Policy';
+$lang['mail_filter_file_extension'] = 'Fájlkiterjesztés';
+$lang['mail_filter_mail_volume'] = 'mail kötet';
+$lang['mail_filter_quarantine'] = 'Karantén';
+$lang['mail_filter_quarantine_policy'] = 'karantén Policy';
+$lang['mail_filter_subject_tag'] = 'Tárgy Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'Tárgy címke érvénytelen.';
+$lang['mail_filter_virus_detected_policy'] = 'Felismert vírus Policy';
