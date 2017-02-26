28dc63716258f3b43d1ed23f5818e19f3efe9c44
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:14:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Mail Filter Engine ger en uppsättning verktyg för att filtrera inkommande och utgående e-postmeddelanden.';
+$lang['mail_filter_app_name'] = 'Post Filter Motor';
+$lang['mail_filter_bad_header_policy'] = 'Bad Header Policy';
+$lang['mail_filter_banned_file_extensions'] = 'Förbjudna filändelser';
+$lang['mail_filter_banned_file_extension_policy'] = 'Förbjudna Filtillägg Policy';
+$lang['mail_filter_discard_policy'] = 'Släng Policy';
+$lang['mail_filter_file_extension'] = 'Filtillägg';
+$lang['mail_filter_mail_volume'] = 'post Volym';
+$lang['mail_filter_quarantine'] = 'Karantän';
+$lang['mail_filter_quarantine_policy'] = 'karantän Policy';
+$lang['mail_filter_subject_tag'] = 'ämne Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'Angående tag är ogiltig.';
+$lang['mail_filter_virus_detected_policy'] = 'Upptäckta virus Policy';
