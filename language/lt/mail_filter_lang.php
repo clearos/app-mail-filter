5669d93aba6600d3bc1334d07e87d5889f68d34f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:10:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Paštas filtras Variklio suteikia pagrindinę įrankių rinkinys filtravimo atvykstamasis ir išvykstamasis laiškus.';
+$lang['mail_filter_app_name'] = 'Pašto filtras Variklis';
+$lang['mail_filter_bad_header_policy'] = 'Blogas antraštė politika';
+$lang['mail_filter_banned_file_extensions'] = 'Banned failų plėtinius';
+$lang['mail_filter_banned_file_extension_policy'] = 'Banned Failo išplėtimas politika';
+$lang['mail_filter_discard_policy'] = 'Atmesti politika';
+$lang['mail_filter_file_extension'] = 'failo išplėtimas';
+$lang['mail_filter_mail_volume'] = 'paštas tomas';
+$lang['mail_filter_quarantine'] = 'karantinas';
+$lang['mail_filter_quarantine_policy'] = 'karantinas politika';
+$lang['mail_filter_subject_tag'] = 'Tema Gairė';
+$lang['mail_filter_subject_tag_invalid'] = 'Tema žymė yra neteisinga.';
+$lang['mail_filter_virus_detected_policy'] = 'Aptikta, kad virusas politika';
