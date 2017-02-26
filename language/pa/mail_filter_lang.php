46a7c956e9f454fc70d6c7e216aa14f76bb8bcbd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:12:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'ਮੇਲ ਫਿਲਟਰ ਇੰਜਣ ਅੰਦਰ ਵੱਲ ਹੈ ਅਤੇ ਬਾਹਰੀ ਈਮੇਲ ਸੁਨੇਹੇ ਫਿਲਟਰ ਲਈ ਸੰਦ ਦੀ ਇੱਕ ਕੋਰ ਸੈੱਟ ਦਿੰਦਾ ਹੈ.';
+$lang['mail_filter_app_name'] = 'ਮੇਲ ਫਿਲਟਰ ਇੰਜਣ';
+$lang['mail_filter_bad_header_policy'] = 'ਮੰਦਾ ਸਿਰਲੇਖ ਨੀਤੀ';
+$lang['mail_filter_banned_file_extensions'] = 'ਤੇ ਪਾਬੰਦੀ ਫਾਇਲ ਐਕਸ਼ਟੇਸ਼ਨ';
+$lang['mail_filter_banned_file_extension_policy'] = 'ਪਾਬੰਦੀਸ਼ੁਦਾ ਫਾਇਲ ਐਕਸ਼ਟੇਸ਼ਨ ਨੀਤੀ';
+$lang['mail_filter_discard_policy'] = 'ਬਰਖਾਸਤ ਨੀਤੀ';
+$lang['mail_filter_file_extension'] = 'ਫਾਇਲ ਐਕਸ਼ਟੇਸ਼ਨ';
+$lang['mail_filter_mail_volume'] = 'ਮੇਲ ਵਾਲੀਅਮ';
+$lang['mail_filter_quarantine'] = 'ਕੁਆਰੰਟੀਨ';
+$lang['mail_filter_quarantine_policy'] = 'ਕੁਆਰੰਟੀਨ ਨੀਤੀ';
+$lang['mail_filter_subject_tag'] = 'ਵਿਸ਼ਾ ਟੈਗ';
+$lang['mail_filter_subject_tag_invalid'] = 'ਵਿਸ਼ਾ ਟੈਗ ਗਲਤ ਹੈ.';
+$lang['mail_filter_virus_detected_policy'] = 'ਪਛਾਣਿਆ ਵਾਇਰਸ ਨੀਤੀ';
