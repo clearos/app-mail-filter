e7ef65fb76d2d2d38a92677980b43a534a668be9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:09:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'ფოსტა ფილტრი ძრავის უზრუნველყოფს ძირითადი კომპლექტი ინსტრუმენტები ფილტრაცია შემომავალი და გამავალი ფოსტის შეტყობინებები.';
+$lang['mail_filter_app_name'] = 'ფოსტა ფილტრაციის ძრავის';
+$lang['mail_filter_bad_header_policy'] = 'Bad Header პოლიტიკა';
+$lang['mail_filter_banned_file_extensions'] = 'აკრძალული ფაილი გაფართოებები';
+$lang['mail_filter_banned_file_extension_policy'] = 'აკრძალული ფაილის გაფართოება პოლიტიკა';
+$lang['mail_filter_discard_policy'] = 'გაუქმება პოლიტიკის';
+$lang['mail_filter_file_extension'] = 'ფაილის გაფართოება';
+$lang['mail_filter_mail_volume'] = 'ფოსტა მოცულობა';
+$lang['mail_filter_quarantine'] = 'კარანტინის';
+$lang['mail_filter_quarantine_policy'] = 'კარანტინის პოლიტიკა';
+$lang['mail_filter_subject_tag'] = 'თემა Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'თემა tag არასწორია.';
+$lang['mail_filter_virus_detected_policy'] = 'აღმოჩენილი ვირუსის პოლიტიკა';
