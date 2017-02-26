0d5a784581e8b0900af52e54ce38600741c99d2a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:14:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'The Mail Filter Engine fana ka konokono sete ya disebediswa tsa ho filtara melaetsa inbound le outbound mail.';
+$lang['mail_filter_app_name'] = 'Mail Filter Engine';
+$lang['mail_filter_bad_header_policy'] = 'Bad Policy Header';
+$lang['mail_filter_banned_file_extensions'] = 'Thibeloa File atolositsoeng';
+$lang['mail_filter_banned_file_extension_policy'] = 'Thibeloa File Policy Extension';
+$lang['mail_filter_discard_policy'] = 'lahle Policy';
+$lang['mail_filter_file_extension'] = 'File Extension';
+$lang['mail_filter_mail_volume'] = 'Mail Volume';
+$lang['mail_filter_quarantine'] = 'arola batho';
+$lang['mail_filter_quarantine_policy'] = 'arola batho Policy';
+$lang['mail_filter_subject_tag'] = 'ho ipeha tlas&#39;a Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'Ho ipeha tlas&#39;a tag tsa ha o sebetse.';
+$lang['mail_filter_virus_detected_policy'] = 'Lemoha kokoana-hloko ea Policy';
