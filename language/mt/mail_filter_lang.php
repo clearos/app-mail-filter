929bbbcac6ee2597517f492b73c55a3e123eb600
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:12:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Il Posta Iffiltra magna jipprovdi sett bażiku ta &#39;għodod għall-filtrazzjoni mail deħlin u dawk ħerġin.';
+$lang['mail_filter_app_name'] = 'Mail Iffiltra magna';
+$lang['mail_filter_bad_header_policy'] = 'Politika Header Bad';
+$lang['mail_filter_banned_file_extensions'] = 'Estensjonijiet File Pprojbiti';
+$lang['mail_filter_banned_file_extension_policy'] = 'Pprojbiti Politika Estensjoni File';
+$lang['mail_filter_discard_policy'] = 'Politika rimi lura';
+$lang['mail_filter_file_extension'] = 'file Estensjoni';
+$lang['mail_filter_mail_volume'] = 'mail Volum';
+$lang['mail_filter_quarantine'] = 'kwarantina';
+$lang['mail_filter_quarantine_policy'] = 'Politika kwarantina';
+$lang['mail_filter_subject_tag'] = 'Tag soġġett';
+$lang['mail_filter_subject_tag_invalid'] = 'tag suġġett huwa invalidu.';
+$lang['mail_filter_virus_detected_policy'] = 'Politika Virus Osservata';
