4c73fa8fb6cc1dbd65d088e13316c85f32f5e89b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:13:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Почтовый фильтр двигателя обеспечивает базовый набор инструментов для фильтрации входящих и исходящих почтовых сообщений.';
+$lang['mail_filter_app_name'] = 'Почтовый фильтр двигателя';
+$lang['mail_filter_bad_header_policy'] = 'Плохая политика заголовка';
+$lang['mail_filter_banned_file_extensions'] = 'Запрещенные расширения файлов';
+$lang['mail_filter_banned_file_extension_policy'] = 'Banned Политика Расширение файла';
+$lang['mail_filter_discard_policy'] = 'Отбросить политика';
+$lang['mail_filter_file_extension'] = 'Расширение файла';
+$lang['mail_filter_mail_volume'] = 'Объем почты';
+$lang['mail_filter_quarantine'] = 'карантинный';
+$lang['mail_filter_quarantine_policy'] = 'Карантин политики';
+$lang['mail_filter_subject_tag'] = 'Тема тегов';
+$lang['mail_filter_subject_tag_invalid'] = 'Тема тег является недействительным.';
+$lang['mail_filter_virus_detected_policy'] = 'Обнаружен политики Вирус';
