998d6cbd2a4f09372752e5e253f1ad6d39a444dc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:16:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Mae&#39;r Engine Hidlo Mail yn darparu set graidd o offer ar gyfer hidlo negeseuon post i mewn ac allan.';
+$lang['mail_filter_app_name'] = 'Engine Filter Mail';
+$lang['mail_filter_bad_header_policy'] = 'Polisi Header drwg';
+$lang['mail_filter_banned_file_extensions'] = 'Estyniadau Ffeil Banned';
+$lang['mail_filter_banned_file_extension_policy'] = 'Gwahardd Polisi Estyniad Ffeil';
+$lang['mail_filter_discard_policy'] = 'Polisi taflu';
+$lang['mail_filter_file_extension'] = 'Estyniad ffeil';
+$lang['mail_filter_mail_volume'] = 'Cyfrol Mail';
+$lang['mail_filter_quarantine'] = 'cwarantin';
+$lang['mail_filter_quarantine_policy'] = 'Polisi Quarantine';
+$lang['mail_filter_subject_tag'] = 'Tag pwnc';
+$lang['mail_filter_subject_tag_invalid'] = 'tag pwnc yn annilys.';
+$lang['mail_filter_virus_detected_policy'] = 'Polisi Virus a ddatryswyd';
