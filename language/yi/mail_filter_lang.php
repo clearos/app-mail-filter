835d9abe881d0f322d401103794e9c1191f5b655
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:16:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'די פּאָסט פֿילטריר ענגינע גיט אַ האַרץ שטעלן פון מכשירים פֿאַר פֿילטרירונג ינבאַונד און אַוטבאַונד פּאָסט אַרטיקלען.';
+$lang['mail_filter_app_name'] = 'מעיל פֿילטריר ענגינע';
+$lang['mail_filter_bad_header_policy'] = 'שלעכט כעדער פּאָליטיק';
+$lang['mail_filter_banned_file_extensions'] = 'באַנד טעקע יקסטענשאַנז';
+$lang['mail_filter_banned_file_extension_policy'] = 'באַנד טעקע געשפּרייט פּאָליטיק';
+$lang['mail_filter_discard_policy'] = 'אַוועקוואַרפן פּאָליטיק';
+$lang['mail_filter_file_extension'] = 'טעקע געשפּרייט';
+$lang['mail_filter_mail_volume'] = 'מעיל באנד';
+$lang['mail_filter_quarantine'] = 'קאַראַנטין';
+$lang['mail_filter_quarantine_policy'] = 'קאַראַנטין פּאָליטיק';
+$lang['mail_filter_subject_tag'] = 'ענין צעטל';
+$lang['mail_filter_subject_tag_invalid'] = 'ענין קוויטל איז פאַרקריפּלט.';
+$lang['mail_filter_virus_detected_policy'] = 'דעטעקטעד ווירוס פּאָליטיק';
