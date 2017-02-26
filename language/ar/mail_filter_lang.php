77635712e011291e57bddcda6a0824694069b700
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:04:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'يوفر محرك تصفية البريد مجموعة أساسية من أدوات لتصفية رسائل البريد الإلكتروني الواردة والصادرة.';
+$lang['mail_filter_app_name'] = 'البريد المحرك تصفية';
+$lang['mail_filter_bad_header_policy'] = 'سياسة رأس سيئة';
+$lang['mail_filter_banned_file_extensions'] = 'ملحقات الملفات المحظورة';
+$lang['mail_filter_banned_file_extension_policy'] = 'حظرت سياسة ملف التمديد';
+$lang['mail_filter_discard_policy'] = 'سياسة تجاهل';
+$lang['mail_filter_file_extension'] = 'امتداد الملف';
+$lang['mail_filter_mail_volume'] = 'حجم البريد';
+$lang['mail_filter_quarantine'] = 'الحجر الزراعي';
+$lang['mail_filter_quarantine_policy'] = 'سياسة الحجر الصحي';
+$lang['mail_filter_subject_tag'] = 'تخضع الكلمات الدلالية';
+$lang['mail_filter_subject_tag_invalid'] = 'العلامة تخضع غير صالحة.';
+$lang['mail_filter_virus_detected_policy'] = 'سياسة الفيروسات الكشف عن';
