57d457faad30e1372786521280f250295c63bbf7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:11:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'മെയിൽ ഫിൽറ്റർ എഞ്ചിൻ ഇൻബൗണ്ട് ആൻഡ് ദൂരസ്ഥലത്തേക്കുള്ള മെയിൽ സന്ദേശങ്ങൾ ഫിൽറ്ററിങിനുള്ള ഉപകരണങ്ങൾ ഒരു കോർ സെറ്റ് നൽകുന്നു.';
+$lang['mail_filter_app_name'] = 'മെയിൽ ഫിൽറ്റർ എഞ്ചിൻ';
+$lang['mail_filter_bad_header_policy'] = 'മോശം ഹെഡ്ഡർ നയം';
+$lang['mail_filter_banned_file_extensions'] = 'നിരോധിച്ച ഫയൽ വിപുലീകരണങ്ങൾ';
+$lang['mail_filter_banned_file_extension_policy'] = 'നിരോധിത ഫയൽ എക്സ്റ്റൻഷൻ നയം';
+$lang['mail_filter_discard_policy'] = 'നയം നിരസിക്കുക';
+$lang['mail_filter_file_extension'] = 'ഫയൽ എക്സ്റ്റൻഷൻ';
+$lang['mail_filter_mail_volume'] = 'മെയിൽ വോളിയം';
+$lang['mail_filter_quarantine'] = 'കപ്പല്വിലക്ക്';
+$lang['mail_filter_quarantine_policy'] = 'നാഡീ നയം';
+$lang['mail_filter_subject_tag'] = 'ടാഗ്';
+$lang['mail_filter_subject_tag_invalid'] = 'ടാഗ് അസാധുവാണ്.';
+$lang['mail_filter_virus_detected_policy'] = 'വൈറസിനെ നയം';
