826f2e9f1b77155e2f4816d13be033105090848d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:16:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'The Engine Mail Hlunga inikeza iqoqo core amathuluzi for gqwa imiyalezo imeyili kwangaphakathi kanye ephumayo.';
+$lang['mail_filter_app_name'] = 'Mail Hlunga Engine';
+$lang['mail_filter_bad_header_policy'] = 'Policy Iheda Bad';
+$lang['mail_filter_banned_file_extensions'] = 'Izandiso Ifayela Banned';
+$lang['mail_filter_banned_file_extension_policy'] = 'Banned Ifayela Extension yemfihlo';
+$lang['mail_filter_discard_policy'] = 'Lahla yemfihlo';
+$lang['mail_filter_file_extension'] = 'Isandiso Ifayela';
+$lang['mail_filter_mail_volume'] = 'Mail Volume';
+$lang['mail_filter_quarantine'] = 'ngokuhlukaniswa';
+$lang['mail_filter_quarantine_policy'] = 'Policy ngokuhlukaniswa';
+$lang['mail_filter_subject_tag'] = 'Tag Ndaba';
+$lang['mail_filter_subject_tag_invalid'] = 'ithegi Isifundo asivumelekile.';
+$lang['mail_filter_virus_detected_policy'] = 'Kutholwe Virus yemfihlo';
