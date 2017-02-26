259fd85bf612bab46e2f3a92960d366824f9de6a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:07:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Η Μηχανή Mail φίλτρο παρέχει ένα βασικό σύνολο εργαλείων για το φιλτράρισμα εισερχόμενων και εξερχόμενων μηνυμάτων ηλεκτρονικού ταχυδρομείου.';
+$lang['mail_filter_app_name'] = 'Μηχανή φίλτρου ταχυδρομείου';
+$lang['mail_filter_bad_header_policy'] = 'Bad Πολιτική Header';
+$lang['mail_filter_banned_file_extensions'] = 'Αποκλεισμένο επεκτάσεις αρχείων';
+$lang['mail_filter_banned_file_extension_policy'] = 'Αποκλεισμένο Πολιτική Επέκταση αρχείου';
+$lang['mail_filter_discard_policy'] = 'Απόρριψη Πολιτική';
+$lang['mail_filter_file_extension'] = 'Επέκταση αρχείου';
+$lang['mail_filter_mail_volume'] = 'Όγκος Mail';
+$lang['mail_filter_quarantine'] = 'Καραντίνα';
+$lang['mail_filter_quarantine_policy'] = 'καραντίνα Πολιτική';
+$lang['mail_filter_subject_tag'] = 'Θέμα Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'Θέμα ετικέτας δεν είναι έγκυρο.';
+$lang['mail_filter_virus_detected_policy'] = 'Εντοπίστηκε Πολιτική Virus';
