696793a514bd967c522ed9aed96550d1c5720a36
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:09:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'The Mail Iyo Engine na-enye a isi set ngwaọrụ maka nzacha inbound na outbound mail ozi.';
+$lang['mail_filter_app_name'] = 'Mail Iyo Engine';
+$lang['mail_filter_bad_header_policy'] = 'Bad nkụnye eji isi mee Policy';
+$lang['mail_filter_banned_file_extensions'] = 'Iwu File Extensions';
+$lang['mail_filter_banned_file_extension_policy'] = 'Iwu File Extension Policy';
+$lang['mail_filter_discard_policy'] = 'tụfuo Policy';
+$lang['mail_filter_file_extension'] = 'File Extension';
+$lang['mail_filter_mail_volume'] = 'mail Mpịakọta';
+$lang['mail_filter_quarantine'] = 'kwarantaini';
+$lang['mail_filter_quarantine_policy'] = 'kwarantaini Policy';
+$lang['mail_filter_subject_tag'] = 'isiokwu Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'Isiokwu mkpado bụ ghara ịdị irè.';
+$lang['mail_filter_virus_detected_policy'] = 'Ahụrụ Virus Policy';
