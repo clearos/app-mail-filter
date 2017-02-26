34f920a281be29721b0962b3ecb8ff4da7da20ce
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:15:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'เครื่องยนต์กรองเมลให้ชุดหลักของเครื่องมือสำหรับการกรองข้อความอีเมลขาเข้าและขา';
+$lang['mail_filter_app_name'] = 'เครื่องยนต์กรองจดหมาย';
+$lang['mail_filter_bad_header_policy'] = 'นโยบายส่วนหัวไม่ดี';
+$lang['mail_filter_banned_file_extensions'] = 'ห้ามนามสกุลไฟล์';
+$lang['mail_filter_banned_file_extension_policy'] = 'นโยบายขยายไฟล์ที่ต้องห้าม';
+$lang['mail_filter_discard_policy'] = 'นโยบายการยกเลิก';
+$lang['mail_filter_file_extension'] = 'ไฟล์นามสกุล';
+$lang['mail_filter_mail_volume'] = 'ปริมาณจดหมาย';
+$lang['mail_filter_quarantine'] = 'กักกัน';
+$lang['mail_filter_quarantine_policy'] = 'นโยบายการกักเก็บ';
+$lang['mail_filter_subject_tag'] = 'แท็กเรื่อง';
+$lang['mail_filter_subject_tag_invalid'] = 'แท็กเรื่องไม่ถูกต้อง';
+$lang['mail_filter_virus_detected_policy'] = 'นโยบายการตรวจพบไวรัส';
