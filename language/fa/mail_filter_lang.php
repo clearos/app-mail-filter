e42e2b8e7a966d8b0319376e97522287b04ee234
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:12:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'موتور ایمیل فیلتر هسته مجموعه ای از ابزار برای فیلتر پیام های پست الکترونیکی ورودی و خروجی فراهم می کند.';
+$lang['mail_filter_app_name'] = 'ایمیل موتور فیلتر';
+$lang['mail_filter_bad_header_policy'] = 'سیاست سربرگ بد';
+$lang['mail_filter_banned_file_extensions'] = 'افزودن فایل ممنوع';
+$lang['mail_filter_banned_file_extension_policy'] = 'سیاست فرمت فایل ممنوع';
+$lang['mail_filter_discard_policy'] = 'سیاست دور';
+$lang['mail_filter_file_extension'] = 'فرمت فایل';
+$lang['mail_filter_mail_volume'] = 'ایمیل جلد';
+$lang['mail_filter_quarantine'] = 'قرنطینه';
+$lang['mail_filter_quarantine_policy'] = 'سیاست قرنطینه';
+$lang['mail_filter_subject_tag'] = 'تم برچسب';
+$lang['mail_filter_subject_tag_invalid'] = 'تم برچسب نامعتبر است.';
+$lang['mail_filter_virus_detected_policy'] = 'سیاست ویروس شناسایی';
