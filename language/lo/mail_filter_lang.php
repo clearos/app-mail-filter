9e019e318c9fbfd4e5d0faed87735e90ca0a002b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:10:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'ວິສະວະກອນ Mail Filter ສະຫນອງທີ່ກໍານົດໄວ້ສໍາຄັນຂອງເຄື່ອງມືສໍາລັບການກັ່ນຕອງຂໍ້ຄວາມ mail ຂາເຂົ້າແລະຂາ.';
+$lang['mail_filter_app_name'] = 'Engine Filter Mail';
+$lang['mail_filter_bad_header_policy'] = 'ນະໂຍບາຍ Header Bad';
+$lang['mail_filter_banned_file_extensions'] = 'ຫ້າມນາມສະກຸນໄຟ';
+$lang['mail_filter_banned_file_extension_policy'] = 'ນະໂຍບາຍການຂະຫຍາຍເອກະສານການປະກາດຫ້າມ';
+$lang['mail_filter_discard_policy'] = 'ນະໂຍບາຍຍົກເລີກ';
+$lang['mail_filter_file_extension'] = 'ການຂະຫຍາຍເອກະສານ';
+$lang['mail_filter_mail_volume'] = 'ປະລິມານ mail';
+$lang['mail_filter_quarantine'] = 'ການກັກກັນ';
+$lang['mail_filter_quarantine_policy'] = 'ນະໂຍບາຍການກັກກັນ';
+$lang['mail_filter_subject_tag'] = 'Tag Subject';
+$lang['mail_filter_subject_tag_invalid'] = 'ໂຄດຄໍາສັ່ງຫົວຂໍ້ບໍ່ຖືກຕ້ອງ.';
+$lang['mail_filter_virus_detected_policy'] = 'ນະໂຍບາຍ Virus ພົບ';
