9fd9f7f5627b69f4f6e62c29b76d2a9726d3151f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:11:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Ny Mail Filter Engine dia manome ny fototra napetraka ny fitaovana noho ny fanivànana mampihiditra sy Mivoaka namany Sary hafatra.';
+$lang['mail_filter_app_name'] = 'Mail Filter Engine';
+$lang['mail_filter_bad_header_policy'] = 'Bad Header Policy';
+$lang['mail_filter_banned_file_extensions'] = 'Voarara File Extensions';
+$lang['mail_filter_banned_file_extension_policy'] = 'Voarara File Extension Policy';
+$lang['mail_filter_discard_policy'] = 'hanary Policy';
+$lang['mail_filter_file_extension'] = 'File Extension';
+$lang['mail_filter_mail_volume'] = 'Mail Volume';
+$lang['mail_filter_quarantine'] = 'ny fanokanan-toerana';
+$lang['mail_filter_quarantine_policy'] = 'ny fanokanan-toerana Policy';
+$lang['mail_filter_subject_tag'] = 'Subject Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'Subject tenifototra dia tsy manan-kery.';
+$lang['mail_filter_virus_detected_policy'] = 'Hita Virus Policy';
