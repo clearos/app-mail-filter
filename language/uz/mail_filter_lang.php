dbaa0c1169a93464f2d71c1066051577a7902bde
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:16:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Mail Filter Dvigatel kelgan va ketgan mail xabarlarini filtrlash uchun vositalar asosiy to`plamini taqdim etadi.';
+$lang['mail_filter_app_name'] = 'Mail Filter mexanizmi';
+$lang['mail_filter_bad_header_policy'] = 'Bad Header siyosati';
+$lang['mail_filter_banned_file_extensions'] = 'Man Fayl kengaytmalari';
+$lang['mail_filter_banned_file_extension_policy'] = 'Man Fayl kengaytma siyosati';
+$lang['mail_filter_discard_policy'] = 'Bekor siyosati';
+$lang['mail_filter_file_extension'] = 'File Extension';
+$lang['mail_filter_mail_volume'] = 'mail Volume';
+$lang['mail_filter_quarantine'] = 'karantin';
+$lang['mail_filter_quarantine_policy'] = 'karantin siyosati';
+$lang['mail_filter_subject_tag'] = 'Mavzu Agarda';
+$lang['mail_filter_subject_tag_invalid'] = 'Mavzu yorlig&#39;i haqiqiy emas.';
+$lang['mail_filter_virus_detected_policy'] = 'Aniqlangan Virus siyosati';
