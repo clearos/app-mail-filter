6b0b6ed2679d9b4298792f9fa3a965ca1789af06
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:14:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Cijeungjing Filter Engine nyadiakeun susunan inti pakakas pikeun Disaring talatah surélék inbound jeung outbound.';
+$lang['mail_filter_app_name'] = 'Surat Filter Engine';
+$lang['mail_filter_bad_header_policy'] = 'Kawijakan Lulugu Bad';
+$lang['mail_filter_banned_file_extensions'] = 'Ekstensi File ngalarang';
+$lang['mail_filter_banned_file_extension_policy'] = 'Ngalarang File Kawijakan penyuluh';
+$lang['mail_filter_discard_policy'] = 'Kawijakan Piceun';
+$lang['mail_filter_file_extension'] = 'file penyuluh';
+$lang['mail_filter_mail_volume'] = 'surat Jilid';
+$lang['mail_filter_quarantine'] = 'karantina';
+$lang['mail_filter_quarantine_policy'] = 'Kawijakan quarantine';
+$lang['mail_filter_subject_tag'] = 'Tag Subject';
+$lang['mail_filter_subject_tag_invalid'] = 'tag Subject téh sah.';
+$lang['mail_filter_virus_detected_policy'] = 'Kawijakan Virus kauninga';
