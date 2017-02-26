add179550955c3993b97b5a455e7c82b8d772bfe
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:10:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'ម៉ាស៊ីនដែលបានផ្តល់នូវសំណុំតម្រងសំបុត្រស្នូលមួយនៃឧបករណ៍សម្រាប់សារតម្រងសំបុត្រស្រុកនិងក្រៅស្រុក។';
+$lang['mail_filter_app_name'] = 'ម៉ាស៊ីនតម្រងសំបុត្រ';
+$lang['mail_filter_bad_header_policy'] = 'គោលនយោបាយបឋមកថាអាក្រក់';
+$lang['mail_filter_banned_file_extensions'] = 'ផ្នែកបន្ថែមឯកសារដែលបានហាមឃាត់';
+$lang['mail_filter_banned_file_extension_policy'] = 'ផ្នែកបន្ថែមឯកសារដែលបានហាមឃាត់គោលនយោបាយ';
+$lang['mail_filter_discard_policy'] = 'គោលនយោបាយបោះបង់';
+$lang['mail_filter_file_extension'] = 'ផ្នែកបន្ថែមឯកសារ';
+$lang['mail_filter_mail_volume'] = 'កម្រិតសំឡេងសំបុត្រ';
+$lang['mail_filter_quarantine'] = 'ការដាក់ឱ្យនៅដាច់ពីគេ';
+$lang['mail_filter_quarantine_policy'] = 'គោលការដាក់ឱ្យនៅដាច់ពីគេ';
+$lang['mail_filter_subject_tag'] = 'ស្លាកប្រធានបទ';
+$lang['mail_filter_subject_tag_invalid'] = 'ស្លាកប្រធានបទមិនត្រឹមត្រូវ។';
+$lang['mail_filter_virus_detected_policy'] = 'គោលការរកឃើញមេរោគ';
