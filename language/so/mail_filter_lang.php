6f94d5a08f749749cfd142deb43eaf829e85804f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:14:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Engine Filter Mail wuxuu bixiyaa set asaasiga ah qalab loogu talagalay dabaqii fariimaha mail inbound iyo outbound.';
+$lang['mail_filter_app_name'] = 'Mail Engine Filter';
+$lang['mail_filter_bad_header_policy'] = 'Siyaasadda Header Bad';
+$lang['mail_filter_banned_file_extensions'] = 'Extensions File mamnuucay';
+$lang['mail_filter_banned_file_extension_policy'] = 'Siyaasadda Extension File mamnuucay';
+$lang['mail_filter_discard_policy'] = 'Tuur Policy';
+$lang['mail_filter_file_extension'] = 'File Extension';
+$lang['mail_filter_mail_volume'] = 'Volume Mail';
+$lang['mail_filter_quarantine'] = 'Karantiinka';
+$lang['mail_filter_quarantine_policy'] = 'Siyaasadda Karantiinka';
+$lang['mail_filter_subject_tag'] = 'Tag Subject';
+$lang['mail_filter_subject_tag_invalid'] = 'tag Subject waa baadil.';
+$lang['mail_filter_virus_detected_policy'] = 'Siyaasadda Virus ogaado';
