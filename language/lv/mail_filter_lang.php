b8562b7fc4bd23c066bf3f9e152551575a03a85e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:11:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/mail_filter_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['mail_filter_app_description'] = 'Pasts Filter Engine nodrošina galveno darbarīku komplektu filtrēšanas ienākošo un izejošo pasta ziņojumus.';
+$lang['mail_filter_app_name'] = 'Pasta filtrs Dzinēja';
+$lang['mail_filter_bad_header_policy'] = 'Bad Header politika';
+$lang['mail_filter_banned_file_extensions'] = 'Aizliegta Failu paplašinājumi';
+$lang['mail_filter_banned_file_extension_policy'] = 'Aizliegumu File Extension politika';
+$lang['mail_filter_discard_policy'] = 'Atmest politika';
+$lang['mail_filter_file_extension'] = 'File Extension';
+$lang['mail_filter_mail_volume'] = 'pasts Volume';
+$lang['mail_filter_quarantine'] = 'karantīna';
+$lang['mail_filter_quarantine_policy'] = 'karantīna politika';
+$lang['mail_filter_subject_tag'] = 'Subject Tag';
+$lang['mail_filter_subject_tag_invalid'] = 'Priekšmets tag ir nederīgs.';
+$lang['mail_filter_virus_detected_policy'] = 'Atklātie Virus politika';
